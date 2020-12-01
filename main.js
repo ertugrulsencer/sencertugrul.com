b115769b5fb13b1cfad776cd5233a9e1043a4930
Ertugrul Sencer <48177057+ertugrulsencer@users.noreply.github.com>
2020-12-01 09:44:49 +0300

    Add files via upload
    Sunum için yeni dosyalar eklendi.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 window.addEventListener('load', () => {
   document.querySelector('#page-loading').style.visibility = "hidden";
   document.querySelector('#page-loading').style.opacity = "0";
+  window.location.href = "#Intro";
 });
 /* STICKY NAVBAR */
 scrollEvents();
@@ -15,3 +16,32 @@ function scrollEvents(){
     navbar.classList.add("sticky")
   }
 }
+/* RESPONSIVE MENU TOGGLE */
+function toggleMenu(){
+    let navbar = document.getElementById('navbar');
+    let menu_toggle_btn = document.getElementById('menuToggle');
+    menu_toggle_btn.classList.toggle('active');
+    navbar.classList.remove('sticky');
+    scrollEvents();
+    if(menu_toggle_btn.className.indexOf('active') > 0 && navbar.className.indexOf('sticky') < 0){
+      navbar.classList.add('sticky');
+    }
+}
+document.getElementById('menuToggleButton').addEventListener('click', (e) => toggleMenu());
+
+/* HIDE MENU */
+function autoHideMenu(){
+  var links = [
+    document.querySelector('#navLinkIntro'),
+    document.querySelector('#navLinkSkills'),
+    document.querySelector('#navLinkPortfolyo'),
+    document.querySelector('#navLinkGaleri'),
+    document.querySelector('#navLinkAboutMe'),
+    document.querySelector('#navLinkContact')
+  ];
+  links.forEach((link) => {
+    console.log('Tıklandııı');
+    link.addEventListener('click', () => toggleMenu());
+  });
+}
+autoHideMenu();
